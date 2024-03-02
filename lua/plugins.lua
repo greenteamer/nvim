e34621c0953d7197ca4f42f1caada09bb5cfc8eb
Aleksander Korovkin <teamer777@gmail.com>
2024-03-02 19:43:02 +0400

    add surrount

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,21 +1,21 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   { 'NLKNguyen/papercolor-theme' },
-  { "folke/neodev.nvim" },
-  { "folke/which-key.nvim" },
+  { 'folke/neodev.nvim' },
+  { 'folke/which-key.nvim' },
   { 'williamboman/mason.nvim' },
   { 'williamboman/mason-lspconfig.nvim'},
   { 'neovim/nvim-lspconfig' },
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
     },
     config = function()
       require('config.nvim-cmp')
@@ -24,13 +24,13 @@ return {
   { 'nvim-treesitter/nvim-treesitter' },
   { 'nkrkv/nvim-treesitter-rescript' },
   {
-    "nvim-neo-tree/neo-tree.nvim",
+    'nvim-neo-tree/neo-tree.nvim',
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
       require('config.neo-tree')
@@ -39,5 +39,15 @@ return {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup({
+          -- Configuration here, or leave empty to use defaults
+      })
+    end
   }
 }
