9e7f17ac88b948ecf486ed6c4d225a5868e2eaca
Aleksander Korovkin <teamer777@gmail.com>
2024-03-03 01:52:43 +0400

    move comment config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -85,21 +85,7 @@ return {
     opts = {
     },
     config = function()
-      require('Comment').setup({
-        toggler = {
-          ---Line-comment toggle keymap
-          line = '<Leader>/',
-          ---Block-comment toggle keymap
-          block = 'gbc',
-        },
-        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-        opleader = {
-          ---Line-comment keymap
-          line = 'gc',
-          ---Block-comment keymap
-          block = '<Leader>/',
-        },
-      })
+      require('config.comment')
     end,
     lazy = false,
   }
